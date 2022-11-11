3bdfa46f5391f1e2460adb394ad6c066e1d14b5e
Emopusta <emreduman225@hotmail.com>
2022-11-11 15:04:13 +0300

    added UserOperationClaim delete command and controller

---
+++ src/project/GymopustaProject/Application/Features/UserOperationClaims/Commands/DeleteUserOperationClaim/DeleteUserOperationClaimCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.UserOperationClaims.Dtos;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Security.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaim
+{
+    public class DeleteUserOperationClaimCommand : IRequest<DeletedUserOperationClaimDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteUserOperationClaimCommandHandler : IRequestHandler<DeleteUserOperationClaimCommand, DeletedUserOperationClaimDto>
+        {
+            private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+
+            public DeleteUserOperationClaimCommandHandler(IUserOperationClaimRepository userOperationClaimRepository)
+            {
+                _userOperationClaimRepository = userOperationClaimRepository;
+            }
+
+            public async Task<DeletedUserOperationClaimDto> Handle(DeleteUserOperationClaimCommand request, CancellationToken cancellationToken)
+            {
+                UserOperationClaim? userOperationClaimToDelete = await _userOperationClaimRepository.GetAsync(uoc => uoc.Id == request.Id);
+
+                if (userOperationClaimToDelete == null) { throw new BusinessException("UserOperationClaim must exist"); }
+
+                UserOperationClaim deletedOperationClaim = await _userOperationClaimRepository.DeleteAsync(userOperationClaimToDelete);
+
+                DeletedUserOperationClaimDto result = new()
+                {
+                    Id = deletedOperationClaim.Id,
+                    OperationClaimId = deletedOperationClaim.OperationClaimId,
+                    UserId = deletedOperationClaim.Id,
+                };
+                return result;
+            }
+        }
+    }
+}
--- src/project/GymopustaProject/WebAPI/Controllers/UserOperationClaimsController.cs
+++ src/project/GymopustaProject/WebAPI/Controllers/UserOperationClaimsController.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Features.Descriptions.Commands.CreateDescription;
 using Application.Features.Descriptions.Dtos;
+using Application.Features.OperationClaims.Commands.DeleteOperationClaim;
+using Application.Features.OperationClaims.Dtos;
 using Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
+using Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaim;
 using Application.Features.UserOperationClaims.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +18,13 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> Add([FromBody] CreateUserOperationClaimCommand createUserOperationClaimCommand)
         {
             CreatedUserOperationClaimDto result = await Mediator.Send(createUserOperationClaimCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteUserOperationClaimCommand deleteUserOperationClaimCommand)
+        {
+            DeletedUserOperationClaimDto result = await Mediator.Send(deleteUserOperationClaimCommand);
             return Ok(result);
         }
     }
