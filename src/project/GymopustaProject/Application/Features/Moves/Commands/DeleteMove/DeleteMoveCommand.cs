c9a8aa92c0d1614d7d1aae328fafc6d786fa0fc9
Emopusta <emreduman225@hotmail.com>
2022-11-11 14:17:51 +0300

    added move delete command

---
+++ src/project/GymopustaProject/Application/Features/Moves/Commands/DeleteMove/DeleteMoveCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.Moves.Dtos;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Moves.Commands.DeleteMove
+{
+    public class DeleteMoveCommand : IRequest<DeletedMoveDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteMoveCommandHandler : IRequestHandler<DeleteMoveCommand, DeletedMoveDto>
+        {
+            private readonly IMoveRepository _moveRepository;
+
+            public DeleteMoveCommandHandler(IMoveRepository moveRepository)
+            {
+                _moveRepository = moveRepository;
+            }
+
+            public async Task<DeletedMoveDto> Handle(DeleteMoveCommand request, CancellationToken cancellationToken)
+            {
+                Move? moveToDelete = await _moveRepository.GetAsync(m => m.Id == request.Id);
+
+                if(moveToDelete == null) { throw new BusinessException("move must exist."); }
+
+                Move deletedMove = await _moveRepository.DeleteAsync(moveToDelete);
+
+                DeletedMoveDto result = new()
+                {
+                    Id = deletedMove.Id,
+                    MoveName = deletedMove.MoveName
+                };
+                return result;
+
+            }
+        }
+    }
+}
---
+++ src/project/GymopustaProject/Application/Features/Moves/Dtos/DeletedMoveDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Moves.Dtos
+{
+    public class DeletedMoveDto 
+    {
+        public int Id { get; set; }
+        public string MoveName { get; set; }
+    }
+}
