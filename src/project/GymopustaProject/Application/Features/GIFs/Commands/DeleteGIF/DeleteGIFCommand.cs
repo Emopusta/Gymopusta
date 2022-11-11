0a88e2ab8855fd7989e0cfe5121d3ff68c982ef2
Emopusta <emreduman225@hotmail.com>
2022-11-11 21:38:20 +0300

    added gif delete command

---
+++ src/project/GymopustaProject/Application/Features/GIFs/Commands/DeleteGIF/DeleteGIFCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.GIFs.Dtos;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GIFs.Commands.DeleteGIF
+{
+    public class DeleteGIFCommand : IRequest<DeletedGIFDto>
+    {
+        public int Id { get; set; }
+
+        public class DeleteGIFCommandHandler : IRequestHandler<DeleteGIFCommand, DeletedGIFDto>
+        {
+            private readonly IGIFRepository _gifRepository;
+
+            public DeleteGIFCommandHandler(IGIFRepository gifRepository)
+            {
+                _gifRepository = gifRepository;
+            }
+
+            public async Task<DeletedGIFDto> Handle(DeleteGIFCommand request, CancellationToken cancellationToken)
+            {
+                GIF? gifToDelete = await _gifRepository.GetAsync(g => g.Id == request.Id);
+
+                if(gifToDelete == null) { throw new BusinessException("gif must exist"); }
+
+                GIF deletedGIF = await _gifRepository.DeleteAsync(gifToDelete);
+
+                DeletedGIFDto result = new()
+                {
+                    Id = deletedGIF.Id,
+                    GIFPath = deletedGIF.GIFPath
+                };
+                return result;
+
+            }
+        }
+    }
+}
---
+++ src/project/GymopustaProject/Application/Features/GIFs/Dtos/DeletedGIFDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GIFs.Dtos
+{
+    public class DeletedGIFDto
+    {
+        public int Id { get; set; }
+        public string GIFPath { get; set; }
+    }
+}
